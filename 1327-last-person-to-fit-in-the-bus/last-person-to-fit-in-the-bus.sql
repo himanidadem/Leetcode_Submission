9091127bf0bd5abde92f870c14f88dd711210555
Himani Dadem <himanidadem@gmail.com>
2026-04-23 16:42:20 -0700

    Time: 782 ms (58.11%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,6 @@
-# Write your MySQL query statement below
-
-with weight_tb as(select person_name, sum(weight) over(order by turn) as total_weight
-from Queue)
-
-select person_name 
-from weight_tb 
+select person_name from
+(select *,sum(weight) over(order by turn) as total_weight 
+from Queue) t
 where total_weight<=1000
 order by total_weight desc
 limit 1
