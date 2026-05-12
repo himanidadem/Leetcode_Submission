f0982b36625c8d84e527e36d6562c48b78a547b5
Himani Dadem <himanidadem@gmail.com>
2026-05-11 22:30:38 -0700

    Time: 316 ms (14.05%) | Memory: 0B (100.00%) - LeetSync

--- solutions/176-second-highest-salary/second-highest-salary.sql
+++ solutions/176-second-highest-salary/second-highest-salary.sql
@@ -1,11 +1,6 @@
-# Write your MySQL query statement below
--- select max(salary) as SecondHighestSalary
--- from (select salary,dense_rank() over(order by salary) as rk
--- from Employee)t
--- where rk=2
-
-select coalesce(
-(select distinct salary 
+select
+coalesce((select salary 
 from Employee
+group by salary
 order by salary desc
 limit 1 offset 1),null) as SecondHighestSalary
